3f026455c997de6ea3843dffe7006af5db324d83
Olubodun <bolowys33@gmail.com>
2024-05-02 19:50:34 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Card from "@/components/Card";
+
 const Dashboard = () => {
-  return ( 
-      <div>
-          Dashboard
-      </div>
-   );
-}
+    return (
+        <div className="mt-5">
+            <div className="flex justify-between gap-5">
+                <Card title="Total Posts" count={500} />
+                <Card title="Posts" count={500} />
+                <Card title="Posts" count={500} />
+            </div>
+        </div>
+    );
+};
 
 export default Dashboard;
---
+++ components/Card.tsx
@@ -0,0 +1,22 @@
+import { LibraryBooks } from "@mui/icons-material";
+import { FC } from "react";
+
+interface CardProps {
+    title: string;
+    count: number;
+    compare?: string;
+}
+
+const Card: FC<CardProps> = ({ title, count }) => {
+    return (
+        <div className="flex gap-5 bg-[#182237] hover:bg-[#2e374a] p-5 rounded-lg cursor-pointer w-full">
+            <LibraryBooks />
+            <div className="space-y-2">
+            <span className="text-sm">{title}</span>
+            <h5 className="text-2xl font-medium">{count}</h5>
+            </div>
+        </div>
+    ) 
+};
+
+export default Card;
