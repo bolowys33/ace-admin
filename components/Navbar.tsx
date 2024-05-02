66553b6dba783f1c956f6d2d14ff2d928a21a247
Olubodun <bolowys33@gmail.com>
2024-05-02 17:47:38 +0100

    Update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { SearchRounded } from "@mui/icons-material";
+import { usePathname } from "next/navigation";
+import SidebarHeader from "./SidebarHeader";
+
 const Navbar = () => {
-    return ( <div>Navbar</div> );
-}
- 
+    const pathname = usePathname();
+
+    return (
+        <div className="flex items-center justify-between rounded-md p-5 bg-[#182237]">
+            <div className="uppercase">{pathname === "/" ? "Dashboard" : pathname}</div>
+            <div className="flex items-center bg-[#2e374a] gap-2 rounded-lg p-2 ">
+                <SearchRounded />
+                <input type="text" title="search" placeholder="Search" className="bg-transparent border-none outline-none pl-2" />
+            </div>
+            <div>
+                <SidebarHeader username="John Doe" role="administrator" />
+            </div>
+        </div>
+    );
+};
+
 export default Navbar;
--- components/SidebarHeader.tsx
+++ components/SidebarHeader.tsx
@@ -8,12 +8,12 @@ interface SidebarHeaderProps {
 const SidebarHeader: React.FC<SidebarHeaderProps> = ({ username, role }) => {
     return (
         <div className="flex items-center space-x-5">
-            <div className="flex justify-center items-center rounded-full h-12 w-12 bg-white text-[#151c2c] font-bold text-4xl">
-                J
-            </div>
             <div className="flex flex-col">
                 <span className="font-bold">{username}</span>
                 <span className="text-xs text-[#b7bac1]">{role}</span>
+            </div>
+            <div className="flex justify-center items-center rounded-full h-12 w-12 bg-white text-[#151c2c] font-bold text-4xl">
+                J
             </div>
         </div>
     );
