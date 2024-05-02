25438deb805af404ed9c0a5912d00ba9c1b04392
Olubodun <bolowys33@gmail.com>
2024-05-02 20:01:20 +0100

    Update

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,13 +7,14 @@ interface CardProps {
     compare?: string;
 }
 
-const Card: FC<CardProps> = ({ title, count }) => {
+const Card: FC<CardProps> = ({ title, count, compare }) => {
     return (
         <div className="flex gap-5 bg-[#182237] hover:bg-[#2e374a] p-5 rounded-lg cursor-pointer w-full">
             <LibraryBooks />
             <div className="space-y-2">
             <span className="text-sm">{title}</span>
             <h5 className="text-2xl font-medium">{count}</h5>
+            <p className="text-xs">{compare}</p>
             </div>
         </div>
     ) 
