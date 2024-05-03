b3c97fe0876b5151370c46b93c2d0efd14dff684
Olubodun <bolowys33@gmail.com>
2024-05-03 10:31:30 +0100

    Update dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Card from "@/components/Card";
 
 const Dashboard = () => {
     return (
-        <div className="mt-5">
+        <div className="mt-3">
             <div className="flex justify-between gap-5">
                 <Card title="Total Posts" count={500} />
                 <Card title="Total Post Impressions" count={500} />
