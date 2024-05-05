6c681ca35aebdd526438e6f8807a64ad25c1e9cd
Olubodun <bolowys33@gmail.com>
2024-05-05 11:01:08 +0100

    Update

--- app/attorneys/page.tsx
+++ app/attorneys/page.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 
 const Attorneys = () => {
     return (
-        <div className="bg-[#182237] my-3 p-5 rounded-lg">
+        <div className="bg-[#182237] my-3 p-5 rounded-lg text-sm h-[500px]">
             <div className="flex items-center justify-between">
                 <Search placeholder="Search for an attorney" />
                 <Link
                     href="/add-attorney"
-                    className="p-2 bg-[#5d57c9] text-white font-medium rounded-md">
+                    className="p-2 bg-[#5d57c9] hover:bg-[#39357e] text-white font-medium rounded-md">
                     Add new
                 </Link>
             </div>
-            <table className="w-full">
+            <table className="w-full my-5">
                 <thead>
                     <tr>
-                        <td className="p-2">Name</td>
-                        <td className="p-2">Role</td>
-                        <td className="p-2">Date added</td>
-                        <td className="p-2">Action</td>
+                        <td className="px-2 pb-3">Name</td>
+                        <td className="px-2 pb-3">Role</td>
+                        <td className="px-2 pb-3">Date added</td>
+                        <td className="px-2 pb-3">Action</td>
                     </tr>
                 </thead>
                 <tbody>
@@ -34,8 +34,16 @@ const Attorneys = () => {
                         <td className="p-2">Founding Partner</td>
                         <td className="p-2">20-12-2024</td>
                         <td className="p-2 space-x-2">
-                            <button type="button" className="py-1 px-2 rounded-md bg-[teal]">View</button>
-                            <button type="button" className="py-1 px-2 rounded-md bg-[crimson]">Delete</button>
+                            <button
+                                type="button"
+                                className="py-1 px-2 rounded-md bg-[teal]">
+                                Edit
+                            </button>
+                            <button
+                                type="button"
+                                className="py-1 px-2 rounded-md bg-[crimson]">
+                                Delete
+                            </button>
                         </td>
                     </tr>
                 </tbody>
