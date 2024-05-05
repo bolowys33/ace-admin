4f9874d4934ebcaa46e52f4c255822366cc3d15f
Olubodun <bolowys33@gmail.com>
2024-05-05 09:53:18 +0100

    Update

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,6 +1,9 @@
-const Search = () => {
+import { SearchRounded } from "@mui/icons-material";
+
+const Search = ({placeholder}: {placeholder: string}) => {
     return ( <div>
-        
+        <SearchRounded />
+        <input type="text" title="search" placeholder={placeholder} className="bg-transparent border-none outline-none pl-2" />
     </div> );
 }
  
