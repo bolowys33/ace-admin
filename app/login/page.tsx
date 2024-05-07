4b8c6340c2456494c74ab905e49cf073a43a9c22
Olubodun <bolowys33@gmail.com>
2024-05-07 13:51:09 +0100

    Update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -97,7 +97,11 @@ const Login = () => {
                         <button
                             type="submit"
                             disabled={isloading}
-                            className="py-2 px-5 bg-[#5d57c9] hover:bg-[#39357e] text-white font-medium rounded-md">
+                            className={`py-2 px-5  text-white font-medium rounded-md ${
+                                isloading
+                                    ? "bg-gray-400 cursor-not-allowed"
+                                    : "bg-[#5d57c9] hover:bg-[#39357e]"
+                            }`}>
                             {isloading ? "Loading" : "Sign-in"}
                         </button>
                     </form>
