6cb5a89012bd833c011cadfea270ee1d80e49cf7
Olubodun <bolowys33@gmail.com>
2024-05-07 13:04:54 +0100

    Update

--- app/posts/add-post/page.tsx
+++ app/posts/add-post/page.tsx
@@ -1,63 +1,74 @@
-"use client";
+    "use client";
 
-import InputField from "@/components/InputField";
-import { Box, Container } from "@mui/material";
-import JoditEditor from "jodit-react";
-import { useRef, useState } from "react";
-import "./jodit-custom.css"; // Import your custom CSS file
-import DOMPurify from "dompurify";
+    import InputField from "@/components/InputField";
+    import { Box, Container } from "@mui/material";
+    import JoditEditor from "jodit-react";
+    import { ChangeEvent, useRef, useState } from "react";
+    import "./jodit-custom.css"; // Import your custom CSS file
+    import DOMPurify from "dompurify";
 
-const AddPost = () => {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
-    const [message, setMessage] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+    const AddPost = () => {
+        const [error, setError] = useState("");
+        const [success, setSuccess] = useState(false);
+        const [isloading, setIsLoading] = useState(false);
 
-    const cleanContent = DOMPurify.sanitize(content);
+        const [inputData, setInputData] = useState({
+            title: "",
+            content: "",
+        });
 
-    const editor = useRef(null);
-    const config = {
-        height: "350px",
-        backgroundColor: "black",
+        const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+            setInputData({ ...inputData, [e.target.name]: e.target.value });
+        };
+
+        const cleanContent = DOMPurify.sanitize(inputData.content);
+
+        const editor = useRef(null);
+        const config = {
+            height: "350px",
+            backgroundColor: "black",
+        };
+
+        return (
+            <div className="bg-[#182237] rounded-lg py-7 min-h-[520px] mt-20">
+                <Container maxWidth="md" className="">
+                    <Box>
+                        <h2 className="text-center font-bold text-3xl">
+                            Create a blog post
+                        </h2>
+                        <form className="flex flex-col items-center md:w-[95%] my-6 mx-auto">
+                            <InputField
+                                type="text"
+                                label="Post title *"
+                                placeholder="Enter post title"
+                                name="title"
+                                value={inputData.title}
+                                required
+                                onChange={handleChange}
+                            />
+                            <div className="w-full space-y-3 my-4">
+                                <label htmlFor="content" className="text-left p-2">
+                                    Post content *
+                                </label>
+                                <JoditEditor
+                                    ref={editor}
+                                    config={config}
+                                    value={inputData.content}
+                                    // onBlur={(newContent) => setContent(newContent)}
+                                    onChange={(newContent) => setInputData({ ...inputData, content: newContent })}
+                                    className="w-full bg-[#2e374a] rounded-lg text-black outline-none"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="py-2 px-5 bg-[#5d57c9] hover:bg-[#39357e] text-white font-medium rounded-md">
+                                Add post
+                            </button>
+                        </form>
+                    </Box>
+                </Container>
+            </div>
+        );
     };
 
-    return (
-        <div className="bg-[#182237] rounded-lg py-7 min-h-[520px] mt-20">
-            <Container maxWidth="md" className="">
-                <Box>
-                    <h2 className="text-center font-bold text-3xl">
-                        Create a blog post
-                    </h2>
-                    <form className="flex flex-col items-center md:w-[95%] my-6 mx-auto">
-                        <InputField
-                            type="text"
-                            label="Post title *"
-                            placeholder="Enter post title"
-                            name="title"
-                            required
-                        />
-                        <div className="w-full space-y-3 my-4">
-                            <label htmlFor="content" className="text-left p-2">
-                                Post content *
-                            </label>
-                            <JoditEditor
-                                ref={editor}
-                                config={config}
-                                value={content}
-                                onBlur={(newContent) => setContent(newContent)}
-                                className="w-full bg-[#2e374a] rounded-lg text-black outline-none"
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="py-2 px-5 bg-[#5d57c9] hover:bg-[#39357e] text-white font-medium rounded-md">
-                            Add post
-                        </button>
-                    </form>
-                </Box>
-            </Container>
-        </div>
-    );
-};
-
-export default AddPost;
+    export default AddPost;
