d9d30144d7e6530b68525328d1f8c9e62fc2a3c9
Olubodun <bolowys33@gmail.com>
2024-05-11 15:41:55 +0100

    Update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,32 +1,34 @@
 import { Container } from "@mui/material";
+import Link from "next/link";
 
 const ProfilePage = () => {
     return (
         <div className="flex flex-col bg-[#182237] my-3 p-5 rounded-lg text-sm min-h-[535px]">
-            <Container maxWidth="sm" className="">
+            <Link
+                    href="/edit-profile"
+                    className="p-2 bg-[#5d57c9] hover:bg-[#39357e] text-white font-medium rounded-md self-end">
+                    Edit profile
+                </Link>
                 <h2 className="text-center font-bold text-3xl mt-5">
                     My Profile
                 </h2>
-                <div className="">
-                    <div>
+            <Container maxWidth="sm" className="bg-[#2e374a] rounded-md mt-8">
+                <div className="space-y-4 mt-7 font-bold">
+                    <div className="p-5 space-x-14">
                         <span>Username: </span>
-                        <span>Bolowys30</span>
+                        <span className="border rounded-md py-2 px-5">Bolowys30</span>
                     </div>
-                    <div>
-                        <span>Username: </span>
-                        <span>Bolowys30</span>
-                    </div>
-                    <div>
+                    <div className="p-5 space-x-8">
                         <span>Email Address: </span>
-                        <span>Bolowys30@gfgdu.com</span>
+                        <span className="border rounded-md py-2 px-5">Bolowys30@gfgdu.com</span>
                     </div>
-                    <div>
+                    <div className="p-5 space-x-14">
                         <span>First Name: </span>
-                        <span>Bolowys30</span>
+                        <span className="border rounded-md py-2 px-5">Bolowys30</span>
                     </div>
-                    <div>
+                    <div className="p-5 space-x-14">
                         <span>Last Name: </span>
-                        <span>Bolowys30</span>
+                        <span className="border rounded-md py-2 px-5">Bolowys30</span>
                     </div>
                 </div>
             </Container>
