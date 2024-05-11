6f60d705e179653457a0ba688e82f4c261cea6c9
Olubodun <bolowys33@gmail.com>
2024-05-11 13:43:06 +0100

    Update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,15 @@
+import { Container } from "@mui/material";
+
 const ProfilePage = () => {
-    return ( 
-        <div>
-            
+    return (
+        <div className="flex flex-col bg-[#182237] my-3 p-5 rounded-lg text-sm min-h-[535px]">
+            <Container>
+                <h2 className="text-center font-bold text-3xl mt-5">
+                    My Profile
+                </h2>
+            </Container>
         </div>
-     );
-}
- 
+    );
+};
+
 export default ProfilePage;
