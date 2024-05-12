561090c75ec4816ec4b3b0b9cc6b49e96da12ab6
Olubodun <bolowys33@gmail.com>
2024-05-12 16:30:04 +0100

    Update

---
+++ components/PasswordForm.tsx
@@ -0,0 +1,136 @@
+import { ChangeEvent, useState } from "react";
+import InputField from "./InputField";
+import { Alert, Box, Container } from "@mui/material";
+import axios from "axios";
+
+const ProfileForm = () => {
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState(false);
+    const [isloading, setIsLoading] = useState(false);
+    const [inputData, setInputData] = useState({
+        currentPassword: "",
+        newPassword: "",
+        confirmPassword: "",
+    });
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputData({ ...inputData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+        setError("");
+        setSuccess(false);
+
+        const token = localStorage.getItem("token");
+
+        const formData = new FormData();
+        formData.append("username", inputData.username);
+        formData.append("email", inputData.email);
+        formData.append("firstname", inputData.firstname);
+        formData.append("lastname", inputData.lastname);
+
+        try {
+            const response = await axios.put("/api/admin", formData, {
+                headers: {
+                    Authorization: token,
+                },
+            });
+
+            if (response.status === 200) {
+                setSuccess(true);
+                setInputData({
+                    currentPassword: "",
+                    newPassword: "",
+                    confirmPassword: "",
+                });
+            } else {
+                setError(response.data.message);
+            }
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                setError(
+                    error.response?.data.message ||
+                        "Error changing password, try again"
+                );
+                setTimeout(() => setError(""), 10000);
+            } else {
+                setError("An unknown error occurred");
+            }
+            setTimeout(() => setError(""), 10000);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <Container maxWidth="sm" className="">
+            <Box>
+                <h2 className="text-center font-bold text-3xl">
+                    Change your password
+                </h2>
+                <div className="text-center w-max mx-auto mt-8">
+                    {error && <Alert severity="error">{error}</Alert>}
+                    {success && (
+                        <Alert severity="success">
+                            Password updated successfully!
+                        </Alert>
+                    )}
+                </div>
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col items-center md:w-[95%] mt-10 mx-auto space-y-3">
+                    <InputField
+                        type="text"
+                        label="Current Password *"
+                        placeholder="Enter current password"
+                        name="currentPassword"
+                        value={inputData.username}
+                        required
+                        onChange={handleChange}
+                    />
+                    <InputField
+                        type="email"
+                        label="Email *"
+                        placeholder="Enter email address"
+                        name="email"
+                        value={inputData.email}
+                        required
+                        onChange={handleChange}
+                    />
+                    <InputField
+                        type="text"
+                        label="First Name *"
+                        placeholder="Enter username"
+                        name="firstname"
+                        value={inputData.firstname}
+                        required
+                        onChange={handleChange}
+                    />
+                    <InputField
+                        type="text"
+                        label="Last Name *"
+                        placeholder="Enter lastname"
+                        name="lastname"
+                        value={inputData.lastname}
+                        required
+                        onChange={handleChange}
+                    />
+                    <button
+                        type="submit"
+                        disabled={isloading}
+                        className={`py-2 px-5  text-white font-medium rounded-md ${
+                            isloading
+                                ? "bg-gray-400 cursor-not-allowed"
+                                : "bg-[#5d57c9] hover:bg-[#39357e]"
+                        }`}>
+                        {isloading ? "Loading" : "Sign-in"}
+                    </button>
+                </form>
+            </Box>
+        </Container>
+    );
+};
+
+export default ProfileForm;
