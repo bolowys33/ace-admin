544c6be716d4b8946b9cfa2c402bce4781927db8
Olubodun <bolowys33@gmail.com>
2024-05-14 14:05:53 +0100

    Update

--- app/attorneys/page.tsx
+++ app/attorneys/page.tsx
@@ -2,10 +2,21 @@
 
 import React, { lazy, Suspense } from "react";
 import Loading from "@/components/Loading";
+import useAuthorization from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
 
 const Attorneys = lazy(() => import("@/components/Attorneys"));
 
 const AttorneysPage = () => {
+    const isAuthenticated = useAuthorization();
+    const router = useRouter();
+
+    if (!isAuthenticated) {
+        // Redirect to the login page
+        router.push("/login");
+        return null; // Optionally, render a loading indicator while redirecting
+    }
+
     return (
         <Suspense fallback={<Loading />}>
             <Attorneys />
