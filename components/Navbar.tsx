f6055bec28f599e0cd98195aedb75e06cb8c81e9
Olubodun <bolowys33@gmail.com>
2024-05-15 18:40:20 +0100

    Update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,12 +8,12 @@ const Navbar = () => {
     const pathname = usePathname();
 
     return (
-        <div className="flex items-center justify-between md:w-4/5 fixed p-5 bg-[#182237] z-50">
-            <div className="uppercase text-[#b7bac1]">{pathname === "/" ? "Dashboard" : pathname}</div>
-            <div className="flex items-center bg-[#2e374a] gap-2 rounded-lg p-2 ">
+        <div className="flex items-center justify-end w-full md:w-4/5 fixed p-5 bg-[#182237] z-50">
+            {/* <div className="uppercase text-[#b7bac1]">{pathname === "/" ? "Dashboard" : pathname}</div> */}
+            {/* <div className="flex items-center bg-[#2e374a] gap-2 rounded-lg p-2 ">
                 <SearchRounded />
                 <input type="text" title="search" placeholder="Search" className="bg-transparent border-none outline-none pl-2" />
-            </div>
+            </div> */}
             <div>
                 <SidebarHeader username="John Doe" role="administrator" />
             </div>
