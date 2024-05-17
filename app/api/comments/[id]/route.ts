c489fcdae73dc9b3675c94c68d5ac120e326f06d
Olubodun <bolowys33@gmail.com>
2024-05-17 19:32:20 +0100

    Update delete handler

--- app/api/comments/[id]/route.ts
+++ app/api/comments/[id]/route.ts
@@ -39,36 +39,39 @@ export async function GET(
 export async function DELETE(
     req: Request,
     { params }: { params: { id: string } }
-  ): Promise<Response> {
+): Promise<Response> {
     try {
-      await connectDB();
-      const { id } = params;
-      const deletedComment = await Comment.findByIdAndDelete(id);
-      if (!deletedComment) {
-        return NextResponse.json(
-          { success: false, message: "Comment not found" },
-          { status: 404 }
+        await connectDB();
+        const { id } = params;
+        const deletedComment = await Comment.findByIdAndDelete(id);
+        if (!deletedComment) {
+            return NextResponse.json(
+                { success: false, message: "Comment not found" },
+                { status: 404 }
+            );
+        }
+
+        // Update the associated Post document to remove the deleted comment reference
+        const post = await Post.findOneAndUpdate(
+            { comments: id },
+            { $pull: { comments: id } },
+            { new: true }
         );
-      }
-  
-      // Update the associated Post document to remove the deleted comment reference
-      const post = await Post.findOneAndUpdate(
-        { comments: id },
-        { $pull: { comments: id } },
-        { new: true }
-      );
-  
-      return NextResponse.json(
-        { success: true, message: "Comment deleted" },
-        { status: 200 }
-      );
+
+        return NextResponse.json(
+            { success: true, message: "Comment deleted" },
+            { status: 200 }
+        );
     } catch (error) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: error instanceof Error ? error.message : "An unknown error occurred",
-        },
-        { status: 500 }
-      );
+        return NextResponse.json(
+            {
+                success: false,
+                message:
+                    error instanceof Error
+                        ? error.message
+                        : "An unknown error occurred",
+            },
+            { status: 500 }
+        );
     }
-  }
+}
