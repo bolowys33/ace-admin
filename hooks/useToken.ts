b52df9618ef5c7404a20d8d817cafd7de84bffaf
Olubodun <bolowys33@gmail.com>
2024-05-17 16:23:49 +0100

    Update

--- hooks/useToken.ts
+++ hooks/useToken.ts
@@ -1,51 +1,50 @@
 import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 
-
 interface TokenResponse {
-  success: boolean;
-  message: string | null;
+    success: boolean;
+    message: string | null;
 }
 
 const useToken = (token: string) => {
-  const [active, setActive] = useState(false)
-  const [isFetching, setIsFetching] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+    const [active, setActive] = useState(false);
+    const [isFetching, setIsFetching] = useState(true);
+    const [error, setError] = useState<string | null>(null);
 
-  const getToken = async () => {
-    setIsFetching(true);
-    setError(null);
+    const getToken = async () => {
+        setIsFetching(true);
+        setError(null);
 
-    try {
-      const { data }: AxiosResponse<TokenResponse> = await axios.get(
-        "/api/recover-password",
-        {
-          headers: {
-            "X-Recovery-Token": token,
-          },
+        try {
+            const { data }: AxiosResponse<TokenResponse> = await axios.get(
+                "/api/recover-password",
+                {
+                    headers: {
+                        "X-Recovery-Token": token,
+                    },
+                }
+            );
+
+            if (data.success) {
+                setActive(true);
+            } else {
+                setActive(false);
+                setError(data.message);
+            }
+        } catch (error) {
+            setError("An error occurred while fetching token.");
+        } finally {
+            setIsFetching(false);
         }
-      );
+    };
 
-      if (data.success) {
-        setActive(true);
-      } else {
-        setActive(false)
-        setError(data.message);
-      }
-    } catch (error) {
-      setError("An error occurred while fetching token.");
-    } finally {
-      setIsFetching(false);
-    }
-  };
+    useEffect(() => {
+        if (token) {
+            getToken();
+        }
+    }, [token]);
 
-  useEffect(() => {
-    if (token) {
-      getToken();
-    }
-  }, [token]);
-
-  return { active, isFetching, error, getToken };
+    return { active, isFetching, error, getToken };
 };
 
 export default useToken;
