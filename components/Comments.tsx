0229aa229268f19044d06a0a35683824de17b9ff
Olubodun <bolowys33@gmail.com>
2024-05-20 14:24:56 +0100

    Update

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -9,31 +9,35 @@ interface CommentProps {
 const Comments: FC<CommentProps> = ({ comments }) => {
     return (
         <Container maxWidth="sm" className="mt-14">
-            <h2 className="text-xl font-bold mb-4 text-center">Comments</h2>
             {comments?.length === 0 ? (
                 <div className="grid place-items-center text-red-500 h-[250px]">
                     No comment found
                 </div>
             ) : (
-                <div className="space-y-4">
-                    {comments?.map((comment) => (
-                        <div
-                            key={comment._id}
-                            className="bg-[#2e374a] p-4 rounded-md">
-                            <div className="flex items-center justify-between mb-2">
-                                <h3 className="font-bold text-[#b7bac1]">
-                                    {comment.author}
-                                </h3>
-                                <p className="text-[#b7bac1] text-sm">
-                                    {new Date(
-                                        comment.date_created
-                                    ).toLocaleString()}
-                                </p>
+                <>
+                    <h2 className="text-xl font-bold mb-4 text-center">
+                        Comments
+                    </h2>
+                    <div className="space-y-4">
+                        {comments?.map((comment) => (
+                            <div
+                                key={comment._id}
+                                className="bg-[#2e374a] p-4 rounded-md">
+                                <div className="flex items-center justify-between mb-2">
+                                    <h3 className="font-bold text-[#b7bac1]">
+                                        {comment.author}
+                                    </h3>
+                                    <p className="text-[#b7bac1] text-sm">
+                                        {new Date(
+                                            comment.date_created
+                                        ).toLocaleString()}
+                                    </p>
+                                </div>
+                                <p className="">{comment.body}</p>
                             </div>
-                            <p className="">{comment.body}</p>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                </>
             )}
         </Container>
     );
